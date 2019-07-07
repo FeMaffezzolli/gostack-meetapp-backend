ea4d1a958d28a4e12d65f3437a59016705d593ad
Felipe Maffezzolli <felipe.maffezzolli@gmail.com>
2019-07-07 16:09:01 -0300

    add sequelize and prepared folders structure

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-	if (req.query.name) {
-		return res.json({ ok: req.query.name });
-	}
-	return res.json({ ok: true });
+routes.get('/', (req, res) => {
+  if (req.query.name) {
+    return res.json({ ok: req.query.name });
+  }
+  return res.json({ ok: true });
 });
 
 export default routes;
